5e6c3ae6975dd05c9ced2679a6a7e2013540ef74
CarlosribesDev <carlosribes.dev@gmail.com>
2023-07-31 00:43:52 +0200

    build: first commit

---
+++ src/main/java/com/bunker/bunkerframework/BunkerFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.bunker.bunkerframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunkerFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BunkerFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bunker/bunkerframework/BunkerFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunker.bunkerframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunkerFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
